63fd72c9b282aed98359da008a90f9d79ba2c758
bimalka98 <bimalkapiyaruwan1998322@gmail.com>
2021-04-03 15:57:32 +0530

    Cash Multiplier done

--- Mini-xtreme-2021/q4.cpp
+++ Mini-xtreme-2021/q4.cpp
@@ -8,10 +8,8 @@ bool canget(ull target, vector<int> & array, map<ull, bool> &memo);
 int main(){
   int t;
   cin >>t; //test cases
-
   vector<int> Multipliers = {10,20};
   map<ull, bool> memo;
-
   for(int i =0; i < t; i++){
     ull final;
     cin >> final;
@@ -19,20 +17,22 @@ int main(){
     if(status) cout << "Yes" << '\n';
     else cout << "No" << '\n';
   }
-  return 0;
+return 0;
 }
 
 bool canget(ull target, vector<int> &array, map<ull, bool> &memo){
 if(memo[target]) return memo[target];
 if(target == 1) return true; // target perfectly divides
+if (target == 0) return false;
 //at least one element returns true it will be enough
 for(int x: array){
   ull remainder = target/x;
-  if (target%x == 0)
-    if(canget(remainder, array, memo)==true) {
+  if (target%x == 0){
+      if(canget(remainder, array, memo)==true) {
       memo[target] = true;
       return memo[target];}
-    }
+  }
+}
 memo[target] = false;
 return false;
 }
--- Mini-xtreme-2021/q5.py
+++ Mini-xtreme-2021/q5.py
@@ -1,22 +1,22 @@
+# Minimizing Cost 1
 t=int(input())
-while(t>0):
-    t-=1
+for i in range(t):
     INF = 1000000000
-    c = list(map(int,input().split()))
-    while 1==1:
+    costs = list(map(int,input().split()))
+    while True:
         reduced = False
         for a in range(10):
             for b in range(a,10):
                 d = (a + b) % 10
-                if c[d] > c[a] + c[b]:
+                if costs[d] > costs[a] + costs[b]:
                     reduced = True
-                    c[d] = c[a] + c[b]
+                    costs[d] = costs[a] + costs[b]
         if not reduced:
             break
     le = int(input())
-    #print(c)
+    #print(costs)
     s=input()
-    tot = 0
+    total = 0
     for i in s:
-        tot += c[ord(i)-48]
-    print(tot)
+        total += costs[int(i)]
+    print(total
